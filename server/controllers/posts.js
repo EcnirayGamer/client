3c48fd30fb77551108fec9eb56b2e66ab7cd1214
Yarince Pascual-Mateo <ypascual@uncc.edu>
2022-08-02 23:47:07 -0400

    Update post -unfinished

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,7 @@
-import PostMessage  from "../models/postMessage.js";
+import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
-    try{
+    try {
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages);
     } catch (error) {
@@ -21,4 +21,16 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+
+    res.json(updatedPost);
+
 }
